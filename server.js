6cbc1b99918ccc78fbb85ad704c75e6225a489a7
Diego Sousa <diegomsousaa@gmail.com>
2023-02-23 17:01:31 -0500

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ const deleteCocktailFromFavorites = require("./modules/deleteCocktailFromFavorit
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 app.use(express.json());
 app.use(verifyUser);
 
